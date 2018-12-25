33117e47aa1534b3469655d2c6896922de9f62b9
Guilherme Torres <gt.borland@gmail.com>
2018-12-25 09:37:26 -0200

    Exercicio babynames

--- mod-1/google-python-exercises/babynames/babynames.py
+++ mod-1/google-python-exercises/babynames/babynames.py
@@ -34,6 +34,10 @@ Suggested milestones for incremental development:
  -Fix main() to use the extract_names list
 """
 
+def extract_files():
+    #baby_names
+    from os import listdir    
+    return listdir('.')
 
 def extract_names(filename):
     """
@@ -42,6 +46,22 @@ def extract_names(filename):
     ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
     """
     # +++your code here+++
+    #year
+    #list_files = extract_files()
+    result = []
+    #for f in list_files:
+    if '.html' in filename:
+        year = re.search('baby(.*?)\.html', filename).group(1)#'1994.html'
+        result.append(year)
+        lines = open(filename, 'r').readlines()
+        for line in lines:        
+            babys = re.findall('<td>(.*?)</td>', line)
+            if (len(babys) == 3):
+                result.append('{} {}'.format(babys[1], babys[0]))
+                result.append('{} {}'.format(babys[2], babys[0]))
+            #print(result)
+    result.sort()
+    print(result)
     return
 
 
@@ -51,20 +71,26 @@ def main():
     # which is the script itself.
     args = sys.argv[1:]
 
-    if not args:
-        print('usage: [--summaryfile] file [file ...]')
-        sys.exit(1)
+    #if not args:
+    #    print('usage: [--summaryfile] file [file ...]')
+    #    sys.exit(1)
 
     # Notice the summary flag and remove it from args if it is present.
-    summary = False
-    if args[0] == '--summaryfile':
-        summary = True
-        del args[0]
+    #summary = False
+    #if args[0] == '--summaryfile':
+    #    summary = True
+    #    del args[0]
 
         # +++your code here+++
         # For each filename, get the names, then either print the text output
         # or write it to a summary file
+    list_files = extract_files()
+    for f in list_files:
+        extract_names(f)
+        #for f in list_files:
+
 
 
 if __name__ == '__main__':
+    extract_names('')
     main()
