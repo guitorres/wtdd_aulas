0132b9cc586560eb0312d1285c724f673031bb09
Guilherme Torres <gt.borland@gmail.com>
2018-12-29 10:08:12 -0200

    Aula M2A08

--- mod-2/fizzbuzz/fizzbuzz.py
+++ mod-2/fizzbuzz/fizzbuzz.py
@@ -1,11 +1,12 @@
 """
 Regras do Fizzbuzz
 
-1. Se a posição for multipla de 3: fizz
-2. Se a posição for multipla de 5: buzz
-3. Se a posição for multipla de 3 e 5: fizzbuzz
-4. Para qualquer outra posição fale o proprio n°
+1. Se a posicao for multipla de 3: fizz
+2. Se a posicao for multipla de 5: buzz
+3. Se a posicao for multipla de 3 e 5: fizzbuzz
+4. Para qualquer outra posicao fale o proprio n
 """
+
 
 from functools import partial
 
@@ -15,7 +16,7 @@ multiple_of_3 = partial(multiple_of, 3)
 
 def robot(pos):
     say = str(pos)
-
+    
     if multiple_of_3(pos) and multiple_of_5(pos) :
         say = 'fizzbuzz'
 
@@ -26,20 +27,3 @@ def robot(pos):
         say = 'fizz'
 
     return say
-
-if __name__ == '__main__':
-    assert robot(1) == '1'
-    assert robot(2) == '2'
-    assert robot(4) == '4'
-
-    assert robot(3) == 'fizz'
-    assert robot(6) == 'fizz'
-    assert robot(9) == 'fizz'
-
-    assert robot(5) == 'buzz'
-    assert robot(10) == 'buzz'
-    assert robot(20) == 'buzz'
-
-    assert robot(15) == 'fizzbuzz'
-    assert robot(30) == 'fizzbuzz'
-    assert robot(45) == 'fizzbuzz'
